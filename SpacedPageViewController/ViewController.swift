d47eea1f6c5dedb4da081d7a3580dc21abe7f29a
Ahmed Khalaf <ahmedkhalaf.92@gmail.com>
2019-05-12 01:46:33 +0200

    Initial Commit

---
+++ SpacedPageViewController/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SpacedPageViewController
+//
+//  Created by Ahmed Khalaf on 5/12/19.
+//  Copyright © 2019 Ahmed Khalaf. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
